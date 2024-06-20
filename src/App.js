797fc45c7522fd6591d406b6c5316e93a3185f16
ahalim150 <ahmedabdelhalim150@gmail.com>
2024-06-20 20:11:30 +0300

    React Qoute Generator with random API qoutes and copy button

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React, {useState} from 'react';
+
+const App = () => {
+  const url = "https://api.quotable.io/random";
+  let quoteData = {
+    content: "Let time be your only competitor.",
+    author: "Ahmed Saber"
+  }
+  const [quote, setQuote] = useState(quoteData)
+
+  const generateQuote = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setQuote(data)
+      });
+  }
+
+  const copy = () => {
+    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
+    alert('copied')
+  }
+
+  return (
+    <>
+      <h1>Quote Generator React App</h1>
+      <div className="container">
+        <p>{quote.content}</p>
+        <span>{quote.author}</span>
+        <div className="btns">
+          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default App;
